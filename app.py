8134efcc8f5479c3193caa2e2e4302d64bfda48c
luuthi <luudinhthi22t.t@gmail.com>
2017-09-21 23:15:38 +0700

    fix app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 #sqlite:///data.db
 #postgres://zyqfvsqtqnnjlt:37e8daf153bb33265bfdcbf8a7
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgres://zyqfvsqtqnnjlt:37e8daf153bb33265bfdcbf8a7')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['CORS_ENABLED'] = True
 app.secret_key = 'thild'
